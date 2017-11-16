0e3ab8b3227168be3076ffd4f974da6629f88c40
tetov <sonya.mamurin@gmail.com>
2017-11-16 21:20:49 +0100

    Made my first tests and they didn't succeed :(

--- ciphers.py
+++ ciphers.py
@@ -4,7 +4,7 @@ import string
 
 A2Z = string.ascii_lowercase
 
-def caesar(encrypted_msg, shift: int):
+def caesar(msg, shift: int):
     """
     Decrypt string using the Caesar cipher
 
@@ -13,20 +13,47 @@ def caesar(encrypted_msg, shift: int):
         shift -- Amount to shift alphabet by.
 
     TODO: Add more alphabet's.
+
+    >>> caesar('bzdrzq', 1)
+    'caesar'
+    >>> caesar('wcjbyl', -20)
+    'cipher'
+    >>> caesar('åäö', 5)
+    Traceback (most recent call last):
+        ...
+    ValueError: Input contains letters not part of selected alphabet
+    >>> caesar('~non-alpha!', 5)
+    Traceback (most recent call last):
+        ...
+    ValueError: Input contains non-alphabetic characters
+    >>> caesar('pvupgsbohf', -1251)
+    'outofrange'
     """
 
-    if encrypted_msg.isalpha() is False:
-        raise ValueError("Input contains characters not supported (yet)")
+    import re
 
-    encrypted_msg = encrypted_msg.lower()
+    msg = msg.lower()
+
+    if msg.isalpha() is False:
+        raise ValueError("Input contains non-alphabetic characters")
+
+    if re.findall(r'[^.a-z]', msg):
+        raise ValueError("Input contains letters not part of selected alphabet")
+
+    # If shift is more than length of alphabet then make it not so >:-)
+    if shift > len(A2Z):
+        shift %= len(A2Z)
+
+    if shift < -len(A2Z):
+        shift = len(A2Z) - shift % len(A2Z)
 
     # Adapted from https://stackoverflow.com/a/8895517
     shifted_a2z = A2Z[shift:] + A2Z[:shift]
     table = str.maketrans(A2Z, shifted_a2z)
 
-    return encrypted_msg.translate(table)
+    return msg.translate(table)
 
-def atbash(encrypted_msg):
+def atbash(msg):
     """
     Decrypts ATBASH encrypted text.
     """
@@ -40,3 +67,7 @@ def vinegere(msg, key, direction):
         key -- String used as key for cipher.
         direction -- Switch to change between encryption or decryption.
     """
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
